f07b056e6a81bd38472f70efd93d3b7006963ba9
Joe Gibbs Politz <joe@cs.brown.edu>
2014-11-29 10:52:06 -0500

    bueno

--- wisdom.js
+++ wisdom.js
@@ -67,7 +67,10 @@
 'code should do, and what it is actually doing.”</i></p>',
 
 '<p>Churchill on execution:</p>' +
-'<p><i>“However beautiful the strategy, you should occasionally look at the results.”</i></p>'
+'<p><i>“However beautiful the strategy, you should occasionally look at the results.”</i></p,
+
+'<p><a href="http://carlos.bueno.org/2014/06/refactoring.html">Carlos Bueno</a> on short-term solutions:</p>' +
+'<p><i>“Ther’s nothing quite as permanent as a temporary fix”</i></p>'
   ]; 
 
   var wisCount = Math.floor(Math.random() * wisdoms.length);
